6c8846413057ea3f04cb65ec3c91e9e315ac77e9
Steven Haryanto <stevenharyanto@gmail.com>
2012-11-07 22:38:12 +0000

    Import of SHARYANTO/Software-Catalog-0.02 from CPAN.
    gitpan-cpan-distribution: Software-Catalog
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/Software-Catalog-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Software/Catalog.pm
+++ lib/Software/Catalog.pm
@@ -8,9 +8,14 @@ use Perinci::Sub::Gen::AccessTable 0.17 qw(gen_read_table_func);
 
 require Exporter;
 our @ISA       = qw(Exporter);
-our @EXPORT_OK = qw(list_software);
+our @EXPORT_OK = qw(
+                       get_software_info
+                       list_software
+               );
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
+
+our %SPEC;
 
 # XXX import catalog from software-catalog
 # (https://github.com/sharyanto/software-catalog)
@@ -18,55 +23,113 @@ my @software = (
     {
         id           => 'wordpress',
         debian_names => [qw/wordpress/],
-        lang         => 'php',
-        tags         => [qw/webapp/],
+        tags         => [qw/
+                               implemented-in::php
+                               interface::web
+                               web::blog
+                           /],
+    },
+    {
+        id           => 'joomla',
+        debian_names => undef,
+        tags         => [qw/
+                               implemented-in::php
+                               interface::web
+                               web::cms
+                           /],
+    },
+    {
+        id           => 'jquery',
+        debian_names => [qw/libjs-jquery/],
+        tags         => [qw/
+                               implemented-in::javascript
+                               devel::library
+                           /],
+    },
+    {
+        id           => 'nginx',
+        debian_names => [qw/nginx/],
+        tags         => [qw/
+                               implemented-in::c
+                               interface::daemon
+                               protocol::http
+                           /],
     },
 );
 
 my $deb_re = qr/\A[a-z0-9]+(-[a-z0-9]+)*\z/;
-my $tag_re = $deb_re;
+my $tag_re = qr/\A([a-z0-9]+(-[a-z0-9]+)*::)?[a-z0-9]+(-[a-z0-9]+)*\z/x;
+
+my $table_spec = {
+    fields => {
+        id => {
+            index      => 0,
+            schema     => ['str*' => {
+                match => $deb_re,
+            }],
+            searchable => 1,
+        },
+        debian_names => {
+            index      => 2,
+            schema     => ['array' => {
+                of => ['str*' => {
+                    match => $deb_re,
+                }],
+            }],
+            sortable   => 0,
+        },
+        tags => {
+            index      => 2,
+            schema     => ['array' => {
+                of => ['str*' => match => $tag_re],
+            }],
+            sortable   => 0,
+        },
+        # XXX field: summary (in various languages)
+        # XXX field: description (in various languages)
+        # XXX field: license
+        # XXX field: url
+
+        # for download_url and releases/latest release, see
+        # Software::Release::Watch
+    },
+    pk => 'id',
+};
 
 my $res = gen_read_table_func(
     name => 'list_software',
     table_data => \@software,
-    table_spec => {
-        fields => {
-            id => {
-                index      => 0,
-                schema     => ['str*' => {
-                    match => $deb_re,
-                }],
-                searchable => 1,
-            },
-            debian_names => {
-                index      => 2,
-                schema     => ['array*' => {
-                    of => ['str*' => {
-                        match => $deb_re,
-                    }],
-                }],
-                sortable   => 0,
-            },
-            tags => {
-                index      => 2,
-                schema     => ['array*' => {
-                    of => ['str*' => match => $tag_re],
-                }],
-                sortable   => 0,
-            },
-            # XXX field: lang
-            # XXX field: summary (in various languages)
-            # XXX field: description (in various languages)
-            # XXX field: license
-            # XXX field: url
-            # XXX field: download_url (see Software::Release::Watch)
-        },
-        pk => 'id',
-    },
+    table_spec => $table_spec,
     langs => ['en_US'],
 );
 die "BUG: Can't generate func: $res->[0] - $res->[1]"
     unless $res->[0] == 200;
+
+$SPEC{get_software_info} = {
+    v => 1.1,
+    summary => 'Get info on a software',
+    args => {
+        id => {
+            summary  => $table_spec->{fields}{id}{summary},
+            schema   => $table_spec->{fields}{id}{schema},
+            req      => 1,
+            pos      => 0,
+        },
+    },
+    "_perinci.sub.wrapper.validate_args" => 0,
+};
+sub get_software_info {
+    my %args = @_;
+    my $id = $args{id}; my $arg_err; ((defined($id)) ? 1 : (($arg_err = 'TMPERRMSG: required data not specified'),0)) && ((!ref($id)) ? 1 : (($arg_err = 'TMPERRMSG: type check failed'),0)) && (($id =~ /(?^:\A[a-z0-9]+(-[a-z0-9]+)*\z)/) ? 1 : (($arg_err = 'TMPERRMSG: clause failed: match'),0)); if ($arg_err) { return [400, "Invalid argument value for id: $arg_err"] } # VALIDATE_ARG
+
+    my $res = list_software("id" => $id, detail=>1);
+    return [404, "No such software"] unless @{$res->[2]};
+
+    [200, "OK", $res->[2][0]];
+}
+
+# XXX get_software_XXX_info (if later on we need more specific info, or when
+# retrieving all info becomes heavy).
 
 1;
 # ABSTRACT: Software catalog
@@ -82,11 +145,11 @@ Software::Catalog - Software catalog
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
- use Software::Catalog qw(list_software);
+ use Software::Catalog qw(list_software get_software_info);
  my $res = list_software();
 
 =head1 DESCRIPTION
@@ -121,6 +184,22 @@ This module has L<Rinci> metadata.
 
 
 None are exported by default, but they are exportable.
+
+=head2 get_software_info(%args) -> [status, msg, result, meta]
+
+Get info on a software.
+
+Arguments ('*' denotes required arguments):
+
+=over 4
+
+=item * B<id>* => I<str>
+
+=back
+
+Return value:
+
+Returns an enveloped result (an array). First element (status) is an integer containing HTTP status code (200 means OK, 4xx caller error, 5xx function error). Second element (msg) is a string containing error message, or 'OK' if status is 200. Third element (result) is optional, the actual result. Fourth element (meta) is called result metadata and is optional, a hash that contains extra information.
 
 =head2 list_software(%args) -> [status, msg, result, meta]
 
